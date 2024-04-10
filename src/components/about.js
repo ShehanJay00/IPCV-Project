51b5320063dd78243f1486313cf9a9b580922ed3
Shehan Jayasooriya <116498102+ShehanJay00@users.noreply.github.com>
2024-04-10 11:08:22 +0530

    Images Added

--- src/components/about.js
+++ src/components/about.js
@@ -1,15 +1,23 @@
 import "./aboutStyles.css";
 import AboutData from "./aboutData";
-import Img1 from "../assets/card1.png";
-import Img2 from "../assets/card2.jpg";
-import Img3 from "../assets/card3.png";
+import Img1 from "../assets/technologies/tf.png";
+import Img2 from "../assets/technologies/keras.webp";
+import Img3 from "../assets/technologies/numpy_3.jpg";
+import Img4 from "../assets/technologies/opencv.png";
+import Img5 from "../assets/technologies/react.png";
+import Img6 from "../assets/technologies/fastApi.png";
+import Img7 from "../assets/technologies/11111.png";
+import Img8 from "../assets/technologies/colab.jpg";
+
+
 
 
 function AboutModel(){
     return(
         <div className="technologies">
-            <h1>OUR SERVICES</h1>
+            <h1>MODEL AND TECHNOLOGIES</h1>
             <p>Our skin analysis and consultation services go beyond surface-level assessments, delving deep into the intricacies of your skin's unique composition. Through advanced technology and expert analysis, we uncover your skin's individual needs, concerns, and potential, providing you with invaluable insights and personalized recommendations tailored to your skin type and goals.</p>
+            
             <div className="work-card">
                 <AboutData
                 image = {Img1}
@@ -26,47 +34,38 @@ function AboutModel(){
                 <AboutData
                 image = {Img3}
                 heading = "NumPy"
+                text=""
+                />
+
+                <AboutData
+                image = {Img4}
+                heading = "OpenCV"
                 text=""
                 />
             </div>
 
             <div className="work-card">
                 <AboutData
-                image = {Img1}
-                heading = "TensorFlow"
+                image = {Img5}
+                heading = "React"
                 text=""
                 />
 
                 <AboutData
-                image = {Img2}
-                heading = "Keras"
+                image = {Img6}
+                heading = "FastAPI"
                 text=""
                 />
 
                 <AboutData
-                image = {Img3}
-                heading = "NumPy"
-                text=""
-                />
-            </div>
-
-
-            <div className="work-card">
-                <AboutData
-                image = {Img1}
-                heading = "TensorFlow"
+                image = {Img7}
+                heading = "Google Cloud Platform"
                 text=""
                 />
 
                 <AboutData
-                image = {Img2}
-                heading = "Keras"
-                text=""
-                />
-
-                <AboutData
-                image = {Img3}
-                heading = "NumPy"
+                image = {Img8}
+                heading = "Google Colaboratory"
                 text=""
                 />
             </div>
